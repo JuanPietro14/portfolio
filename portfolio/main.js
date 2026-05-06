c5c2a374ce9f4960c3dcb1df19fd7767ff4768e8
Juan Pietro <116856078+JuanPietro14@users.noreply.github.com>
2026-05-06 09:13:02 -0300

    Update main.js

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,18 +1,70 @@
-// Animação ao rolar (fade-in)
-const elements = document.querySelectorAll('.card');
+//scroll suave
+const lenis = new Lenis({
+  lerp: 0.1,
+  smoothWheel: true,
+  syncTouch: true
+});
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.style.opacity = 1;
-      entry.target.style.transform = "translateY(0)";
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    
+    if (targetElement) {
+      lenis.scrollTo(targetElement, {
+        offset: -20,
+        duration: 1.4
+      });
     }
   });
 });
 
-elements.forEach(el => {
-  el.style.opacity = 0;
-  el.style.transform = "translateY(20px)";
-  el.style.transition = "0.5s";
-  observer.observe(el);
-});
+
+
+//digitando
+const initTypingEffect = () => {
+  const textElement = document.getElementById("typing");
+  if (!textElement) return;
+
+  const text = "Juan Pietro";
+  let index = 0;
+  let isDeleting = false;
+  let typingSpeed = 200; 
+
+  const typeEffect = () => {
+    const currentText = text.slice(0, index);
+    textElement.innerHTML = currentText; 
+    
+    if (!isDeleting && index <= text.length) {
+      index++;
+      typingSpeed = 130; 
+    } else if (isDeleting && index >= 0) {
+      index--;
+      typingSpeed = 100; 
+    }
+
+    if (index > text.length) {
+      isDeleting = true;
+      typingSpeed = 1500; 
+    } else if (index === 0) {
+      isDeleting = false;
+      typingSpeed = 800; 
+    }
+
+    setTimeout(typeEffect, typingSpeed);
+  };
+
+  typeEffect();
+};
+
+document.addEventListener('DOMContentLoaded', initTypingEffect);
+
+
+document.getElementById('current-year').textContent = new Date().getFullYear();
